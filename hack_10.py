c855cf1070f7099859eaab18a5f99d9b6dd60210
Andrea <andreamigoatencio@gmail.com>
2024-05-13 23:18:42 -0400

    feat: hack resuelto

--- hack_10.py
+++ hack_10.py
@@ -4,5 +4,13 @@ text: "fooziman" output => ["F","0","0","Z","1","M","@","N"]
 
 def fn_hack_10():
     result = "fooziman"
-    #...
-    return result  
+    result = result.replace("a","@")
+    result = result.replace("o","0")
+    result = result.replace("i","1")
+    result = result.upper()
+    result = list(result)
+    return result
+
+
+
+fn_hack_10()
--- hack_7.py
+++ hack_7.py
@@ -4,5 +4,13 @@ loop: while [] output => [5,4,3,2,1,0]
 
 def fn_hack_7():
     result = []
-    #...
+    contador = 0
+    inicio = 6
+    while contador < 6:
+        inicio = inicio - 1
+        result.append(inicio)
+        contador = contador + 1
     return result  
+
+fn_hack_7()
+
